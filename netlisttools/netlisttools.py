bf03636128bb5733f12b893363024cb6145eb01b
sjeapes <sjeapes@gmail.com>
2015-08-06 22:16:35 +0100

    Added power rail detect

--- netlisttools/netlisttools.py
+++ netlisttools/netlisttools.py
@@ -6,10 +6,17 @@
 # Author: Steve Jeapes (netlist@arcoarena.co.uk)
 # Creation Date: 6th Aug 15
 #==============================================================================
+import re
 
 def orphan_nets(netlist):
     orphans = []
     for net in netlist:
         if len(net[1]) < 2:
             orphans.append(net)
+            
+def identify_power(netlist):
+    power = []
+    for net in netlist:
+        if re.match(r'[+-]*[0-9.]*[vV][0-9]*',net):
+            power.append(net)
             
